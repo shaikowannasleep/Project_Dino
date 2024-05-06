dd3055d9d536de32cce2d17574a3f7e07f6547dd
DinDun <34118217+shaikowannasleep@users.noreply.github.com>
2024-05-06 15:36:54 +0700

    Complete Phase 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    public Animator myAnim;
+
+    public static PlayerController instance;
+
+    public string areaTransitionName;
+
+    Vector3 bottomLeftLimit;
+    Vector3 topRightLimit;
+
+    public bool canMove = true;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+        canMove = true;
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
+
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            if (canMove)
+            {
+                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
+
+    }
+
+    public void SetBounds(Vector3 bottomLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = bottomLeft + new Vector3(0.5f, 1f, 0f);
+        topRightLimit = topRight + new Vector3(-0.5f, -1f, 0f);
+    }
+}
