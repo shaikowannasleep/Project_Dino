3f5cd58a60a9559004ec3ce7da4855446ef629c3
DinDun <34118217+shaikowannasleep@users.noreply.github.com>
2024-05-12 18:38:37 +0700

    Player Controller x Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,63 +9,17 @@ public class PlayerController : MonoBehaviour
 
     public Animator myAnim;
 
-    public static PlayerController instance;
-
-    public string areaTransitionName;
-
-    Vector3 bottomLeftLimit;
-    Vector3 topRightLimit;
-
-    public bool canMove = true;
-
-    void Start()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            if (instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
-
-        DontDestroyOnLoad(gameObject);
-        canMove = true;
-    }
-
     void Update()
     {
-        if (canMove)
-        {
-            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-        }
-        else
-        {
-            theRB.velocity = Vector2.zero;
-        }
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
 
         myAnim.SetFloat("moveX", theRB.velocity.x);
         myAnim.SetFloat("moveY", theRB.velocity.y);
 
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            if (canMove)
-            {
-                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
-            }
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
         }
-
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
-
-    }
-
-    public void SetBounds(Vector3 bottomLeft, Vector3 topRight)
-    {
-        bottomLeftLimit = bottomLeft + new Vector3(0.5f, 1f, 0f);
-        topRightLimit = topRight + new Vector3(-0.5f, -1f, 0f);
     }
 }
